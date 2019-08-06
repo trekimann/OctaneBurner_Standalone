44543dc9921bbd2969c5262c7abab923ad463208
Nicholas Mann <mannn@hastingsdirect.com>
2019-08-06 12:24:27 +0100

    Changed Spinner to a flex display

--- src/app/components/spinner.tsx
+++ src/app/components/spinner.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styled, {keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 import octIcon from "./../assets/octaneIcon.png";
 
 const rotate = keyframes`
@@ -16,13 +16,17 @@ const Rotate = styled.div`
   animation: ${rotate} 3s linear infinite;
   width: 50%;
   max-width: 400px;
-  position: absolute;
-  top: 30%;
-  left: 25%;
   `;
 
-export class Spinner extends React.Component{
-    public render() {
-        return <Rotate> <img width="100%" src={octIcon} alt = "Loading"/></Rotate>;
-    }
+const oStyle = {
+  alignItems: "center",
+  display: "flex",
+  height: "90vh",
+  justifyContent: "center",
+};
+
+export class Spinner extends React.Component {
+  public render() {
+    return <div style={oStyle}><Rotate> <img width="100%" src={octIcon} alt="Loading" /></Rotate></div>;
+  }
 }
